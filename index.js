d90e109e25866b386265c4e45d942bec97fe9404
Yohei Yamaguchi <yyamaguchi@r.recruit.co.jp>
2015-07-25 17:38:09 +0900

    make the script work

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 "use strict";
 
-var scraper = require('scraper')
+var QIITA_URL = "http://qiita.com/";
 
-var QIITA_URL = "http://qiita.com/"
+var cheerio = require('cheerio');
+var request = require('request');
+var Promise = require('bluebird');
 
-var QiitaScraper = {}
+var users = ["gologo13", "ktsukago",
+"dai___chi",
+"uny",
+"m_nakamura145",
+"haijima",
+"nabenabeq",];
+var results = [];
 
-/**
- * execute scraping for Qiita
- */
-QiitaScraper.scrape = function(accounts, targets) {
-  scraper(QIITA_URL + "/gologo13", function(err, $) {
-    if (err) {
-      throw err;
-    }
-    console.log($)
+Promise
+.settle(users.map(function(user) {
+  return new Promise(function (resolve, reject) {
+    request(QIITA_URL + user, function(err, res, body) {
+      if (err) return reject(err);
+
+      var $ = cheerio.load(body);
+
+      return resolve({
+        user: user,
+        score: $('span.userPage_contributionCount').text()
+      });
+    });
   });
-}
-
-module.exports = QiitaScraper
+}))
+.then(function(results) {
+  results
+  .filter(function(result) {
+    return result.isFulfilled();
+  })
+  .map(function(result) {
+    return result.value();
+  })
+  .sort(function(a, b) {
+    return parseInt(a["score"], 10) < parseInt(b["score"], 10);
+  })
+  .forEach(function(result) {
+    console.log("%s\t%d", result.user, result.score);
+  });
+})
+.catch(function(err) {
+  console.log(err);
+});
