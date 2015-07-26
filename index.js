ff0e54b405247318a29162ca153ec5da3b7dbb72
Yohei Yamaguchi <joker13meister@gmail.com>
2015-07-27 00:58:42 +0900

    add the feature to show a ranking from an organization

--- index.js
+++ index.js
@@ -8,32 +8,48 @@ var Promise = require('bluebird');
 
 var argv = require('minimist')(process.argv.slice(2));
 
-if (!argv.u) {
-  return console.log("Usage: node index.js -u user1,user2,user3");
+if (!argv.u && !argv.o) {
+  console.log("Usage: qiitarace -u user1,user2,...");
+  console.log("Usage: qiitarace -o organization");
+  return
 }
 
-var users = argv.u.split(",");
-
-Promise
-.settle(users.map(function(user) {
-  return new Promise(function (resolve, reject) {
-    request(QIITA_URL + user, function(err, res, body) {
+new Promise(function(resolve, reject) {
+  if (argv.u) {
+    return resolve(argv.u.split(","));
+  }
+  if (argv.o) {
+    request(QIITA_URL + "organizations/" + argv.o, function(err, res, body) {
       if (err) return reject(err);
 
       var $ = cheerio.load(body);
+      return resolve($('img.organizationMemberIcons_icon').map(function(i, elem) {
+        return $(this).attr('title');
+      }).get());
+    });
+  }
+})
+.then(function(users) {
+  return Promise.settle(users.map(function(user) {
+    return new Promise(function(resolve, reject) {
+      request(QIITA_URL + user, function(err, res, body) {
+        if (err) return reject(err);
 
-      return resolve({
-        user: user,
-        score: parseInt($('span.userPage_contributionCount').text(), 10)
+        var $ = cheerio.load(body);
+
+        return resolve({
+          user: user,
+          score: parseInt($('span.userPage_contributionCount').text(), 10)
+        });
       });
     });
-  });
-}))
+  }))
+})
 .then(function(results) {
   results
   .filter(function(result) {
-    return result.isFulfilled();
-  })
+      return result.isFulfilled();
+    })
   .map(function(result) {
     return result.value();
   })
